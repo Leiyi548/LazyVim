9b7adeb2898798f50874bc5ab141cd8630a48657
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-02-28 21:03:20 +0800

    dashboard 按钮更新 telescope action

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -24,11 +24,11 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+          { action = 'lua require("utils.fancy_telescope").findFile()',                                     desc = " Find file",       icon = " ", key = "f" },
           { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+          { action = 'lua require("utils.fancy_telescope").findRecentFile()',    desc = " Recent files",    icon = " ", key = "r" },
           { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("utils.fancy_telescope").findConfigFile()',    desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
           { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
