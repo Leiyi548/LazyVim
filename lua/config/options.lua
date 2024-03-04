1f24ceb3163c636decbc873d0ef2c3eba0b7b6c3
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-04 22:37:53 +0800

    关闭垂直光标显示

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.cursorcolumn = true
+vim.opt.cursorcolumn = false
 vim.opt.background = "dark"
 vim.opt.backup = false
 vim.opt.writebackup = false
