a12d281ec1d9f316650b89ca21bb42b8c9abdbd1
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-09 21:53:08 +0800

    flash-zhh.nvim remote 正常运行

--- lua/plugins/flash-zhh.lua
+++ lua/plugins/flash-zhh.lua
@@ -4,10 +4,18 @@ return {
     dependencies = "Leiyi548/flash.nvim",
     keys = {
       {
-        "S",
-        mode = { "n" },
+        ";s",
+        mode = { "n", "x" },
         function()
           require("flash-zhh").jump()
+        end,
+        desc = "Flash between Chinese(tiger code)",
+      },
+      {
+        ";s",
+        mode = { "o" },
+        function()
+          require("flash-zhh").remote()
         end,
         desc = "Flash between Chinese(tiger code)",
       },
