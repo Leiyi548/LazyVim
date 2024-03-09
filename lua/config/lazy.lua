4501a20ada55e67426ddde85abf93206a825f55b
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-10 00:38:04 +0800

    import lazyvim.plugins.extras.lang.json

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     -- import/override with your plugins
     { import = "plugins" },
