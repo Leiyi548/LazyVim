da745583d1ef06323f1c92b600660ab77bd895d8
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-09 22:45:45 +0800

    使用 <leader>gp 来 push commit

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
   keys = {
     -- stylua: ignore start
     { "<leader>gf", "<cmd>G<cr>", desc = "open fugitive window", },
+    { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push", },
     -- stylua: ignore end
   },
 }
