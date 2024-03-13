e5df2364eb6de2f1ef1d7d3b83fc7c783813e93c
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-13 19:00:47 +0800

    add new plugin nvim-tabline

---
+++ lua/plugins/nvim-tabline.lua
@@ -0,0 +1,16 @@
+return {
+  "crispgm/nvim-tabline",
+  event = "VeryLazy",
+  config = function()
+    require("tabline").setup({
+      show_index = true, -- show tab index
+      show_modify = true, -- show buffer modification indicator
+      show_icon = false, -- show file extension icon
+      fnamemodify = ":t", -- file name modifier
+      modify_indicator = "[+]", -- modify indicator
+      no_name = "No name", -- no name buffer name
+      brackets = { "[", "]" }, -- file name brackets surrounding
+      inactive_tab_max_length = 0, -- max length of inactive tab titles, 0 to ignore
+    })
+  end,
+}
