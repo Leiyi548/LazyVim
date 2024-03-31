01502c06580d44ccadfe59a3f89848a487de837f
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-03-31 18:58:38 +0800

    disable mini.pairs

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,6 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = true },
-  { "echasnovski/mini.pairs", enabled = true },
+  { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
   -- { "folke/flash.nvim", enabled = false },
 }
