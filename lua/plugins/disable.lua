1560e5b8ba1b101235b5c89facfff825e5c3fd3b
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-01 11:25:54 +0800

    enable mini.surround

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,6 +1,5 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = true },
   { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  -- { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.surround", enabled = true },
 }
