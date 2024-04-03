6e9498cc5ef2256634e0fb7cf31ade398e32fa6b
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-03 14:51:27 +0800

    enable incline will make neovim slow

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,3 +1,20 @@
+local field_format = {
+  name = {
+    guifg = "#a0a0a0",
+    guibg = "#282828",
+  },
+  num = {
+    guifg = "#968c81",
+  },
+  modified = {
+    guifg = "#d6991d",
+  },
+  blocks = {
+    gui = "bold",
+    guifg = "#070707",
+  },
+}
+
 return {
   {
     "b0o/incline.nvim",
@@ -15,15 +32,23 @@ return {
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
-          local buffer = {
+          -- buffer name
+          local buffullname = vim.api.nvim_buf_get_name(props.buf)
+          local bufname_t = vim.fn.fnamemodify(buffullname, ":t")
+          local bufname = (bufname_t and bufname_t ~= "") and bufname_t or "[No Name]"
+          local display_bufname = vim.tbl_extend("force", { bufname, " " }, field_format.name)
+          local modified_icon = {}
+          if vim.api.nvim_get_option_value("modified", { buf = props.buf }) then
+            modified_icon = vim.tbl_extend("force", { "● " }, field_format.modified)
+            display_bufname.guifg = field_format.modified.guifg
+          end
+          local window_number = { " " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" }
+          return {
+            window_number,
             ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold,italic" or "bold" },
-            " ",
-            { " " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+            display_bufname,
+            modified_icon,
           }
-          return buffer
         end,
       })
     end,
