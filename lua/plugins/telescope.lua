08fae87d4bf517f85b6b8656755d8c57d9bdc568
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-04 22:06:25 +0800

    projectroot use pattern instead of lsp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,7 +25,7 @@ return {
         -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
         -- order matters: if one is not detected, the other is used as fallback. You
         -- can also delete or rearangne the detection methods.
-        detection_methods = { "lsp", "pattern" },
+        detection_methods = { "pattern" },
         -- All the patterns used to detect root dir, when **"pattern"** is in
         -- detection_methods
         patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
