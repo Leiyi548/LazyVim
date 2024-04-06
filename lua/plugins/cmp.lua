a722e24e6eb17796b1160a5b6233878909e4e02d
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-06 23:06:40 +0800

    cmp use c-d c-u to scroll_docs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -40,8 +40,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<Space>"] = cmp.mapping(require("cmp_im").select(), { "i" }),
         ["<C-e>"] = cmp.mapping.abort(),
