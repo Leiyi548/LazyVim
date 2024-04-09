5e7b255a1fd3e119b2ee123b844acaaf6acb7669
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-09 18:14:40 +0800

    使用 ctrl-k 来打开 Trouble 页面,不知道 ctrl-t 被谁占用了

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -173,7 +173,6 @@ return {
         end,
         mappings = {
           i = {
-            ["<c-q>"] = open_with_trouble,
             ["<c-s>"] = flash,
             ["<a-t>"] = open_selected_with_trouble,
             ["<a-i>"] = find_files_no_ignore,
@@ -186,7 +185,7 @@ return {
             ["<Right>"] = actions.preview_scrolling_right,
           },
           n = {
-            ["<c-q>"] = open_with_trouble,
+            ["<c-k>"] = open_with_trouble,
             ["q"] = actions.close,
             ["<c-s>"] = flash,
             ["<C-b>"] = actions.preview_scrolling_left,
