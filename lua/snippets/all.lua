13b4fd8a687c8d00d0b502d3f323a38d955d509d
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-12 16:31:36 +0800

    把虎码的;+英文字母补全

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -1,8 +1,6 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local i = ls.insert_node
 local t = ls.text_node
-local ms = ls.multi_snippet
 
 -- enable luasnip autosnippet
 ls.config.setup({ enable_autosnippets = true })
@@ -160,5 +158,161 @@ ls.add_snippets("all", {
     end,
   }, {
     t("|"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";u",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("~"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";i",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("——"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";o",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("〖"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";p",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("〗"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";h",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("『"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";j",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("』"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";d",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("、"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";q",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("：“"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";k",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("￥"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";l",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("%"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";n",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("「"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";m",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("」"),
+  }),
+})
+
+ls.add_snippets("all", {
+  s({
+    trig = ";y",
+    snippetType = "autosnippet",
+    condition = function()
+      return require("cmp_im").getStatus()
+    end,
+  }, {
+    t("·"),
   }),
 })
