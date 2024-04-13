a9bb1d6a69628f1f8204364ee7a0b8da46003634
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-13 18:16:01 +0800

    使用 lsp-progress 在 lualine 显示 lsp 加载

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -91,3 +91,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = event.buf, silent = true })
   end,
 })
+
+-- listen lsp-progress event and refresh lualine
+vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  group = "lualine_augroup",
+  pattern = "LspProgressStatusUpdated",
+  callback = require("lualine").refresh,
+})
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,8 +17,10 @@ local project = {
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    "letieu/harpoon-lualine",
-    enabled = false,
+    "linrongbin16/lsp-progress.nvim",
+    config = function()
+      require("lsp-progress").setup()
+    end,
   },
   event = "VeryLazy",
   init = function()
@@ -78,6 +80,11 @@ return {
         lualine_x = {
           {
             function()
+              return require("lsp-progress").progress()
+            end,
+          },
+          {
+            function()
               return ""
             end,
             color = function()
@@ -113,17 +120,6 @@ return {
         },
         lualine_y = { "progress" },
         lualine_z = { "location" },
-        -- lualine_z = {
-        --   "another_item",
-        --   {
-        --     "harpoon2",
-        --     icon = "",
-        --     indicators = { "1", "2", "3", "4" },
-        --     -- active_indicators = { "壹", "贰", "叁", "肆" },
-        --     active_indicators = { "[1]", "[2]", "[3]", "[4]" },
-        --     separator = "",
-        --   },
-        -- },
       },
       inactive_sections = {
         lualine_a = { window_number },
