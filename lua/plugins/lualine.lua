1c162360f045d57f6ff7ad65621fa7c5299b3ad7
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-23 17:23:20 +0800

    test nvim-navic plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -19,55 +19,106 @@ local project = {
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    "linrongbin16/lsp-progress.nvim",
-    config = function()
-      require("lsp-progress").setup({
-        client_format = function(client_name, spinner, series_messages)
-          if #series_messages == 0 then
-            return nil
-          end
-          return {
-            name = client_name,
-            body = spinner .. " " .. table.concat(series_messages, ", "),
-          }
-        end,
-        format = function(client_messages)
-          --- @param name string
-          --- @param msg string?
-          --- @return string
-          local function stringify(name, msg)
-            return msg and string.format("%s %s", name, msg) or name
-          end
+    {
+      "linrongbin16/lsp-progress.nvim",
+      config = function()
+        require("lsp-progress").setup({
+          client_format = function(client_name, spinner, series_messages)
+            if #series_messages == 0 then
+              return nil
+            end
+            return {
+              name = client_name,
+              body = spinner .. " " .. table.concat(series_messages, ", "),
+            }
+          end,
+          format = function(client_messages)
+            --- @param name string
+            --- @param msg string?
+            --- @return string
+            local function stringify(name, msg)
+              return msg and string.format("%s %s", name, msg) or name
+            end
 
-          local sign = "" -- nf-fa-gear \uf013
-          local lsp_clients = vim.lsp.get_active_clients()
-          local messages_map = {}
-          for _, climsg in ipairs(client_messages) do
-            messages_map[climsg.name] = string.match(climsg.body, "(%d+%%)")
-          end
+            local sign = "" -- nf-fa-gear \uf013
+            local lsp_clients = vim.lsp.get_active_clients()
+            local messages_map = {}
+            for _, climsg in ipairs(client_messages) do
+              messages_map[climsg.name] = string.match(climsg.body, "(%d+%%)")
+            end
 
-          if #lsp_clients > 0 then
-            table.sort(lsp_clients, function(a, b)
-              return a.name < b.name
-            end)
-            local builder = {}
-            for _, cli in ipairs(lsp_clients) do
-              if type(cli) == "table" and type(cli.name) == "string" and string.len(cli.name) > 0 then
-                if messages_map[cli.name] then
-                  table.insert(builder, stringify(cli.name, messages_map[cli.name]))
-                else
-                  table.insert(builder, stringify(cli.name))
+            if #lsp_clients > 0 then
+              table.sort(lsp_clients, function(a, b)
+                return a.name < b.name
+              end)
+              local builder = {}
+              for _, cli in ipairs(lsp_clients) do
+                if type(cli) == "table" and type(cli.name) == "string" and string.len(cli.name) > 0 then
+                  if messages_map[cli.name] then
+                    table.insert(builder, stringify(cli.name, messages_map[cli.name]))
+                  else
+                    table.insert(builder, stringify(cli.name))
+                  end
                 end
               end
+              if #builder > 0 then
+                return sign .. " " .. table.concat(builder, ", ")
+              end
             end
-            if #builder > 0 then
-              return sign .. " " .. table.concat(builder, ", ")
-            end
-          end
-          return ""
-        end,
-      })
-    end,
+            return ""
+          end,
+        })
+      end,
+    },
+    {
+      "SmiteshP/nvim-navic",
+      config = function()
+        require("nvim-navic").setup({
+          icons = {
+            File = "󰈙 ",
+            Module = " ",
+            Namespace = "󰌗 ",
+            Package = " ",
+            Class = "󰌗 ",
+            Method = "󰆧 ",
+            Property = " ",
+            Field = " ",
+            Constructor = " ",
+            Enum = "󰕘",
+            Interface = "󰕘",
+            Function = "󰊕 ",
+            Variable = "󰆧 ",
+            Constant = "󰏿 ",
+            String = "󰀬 ",
+            Number = "󰎠 ",
+            Boolean = "◩ ",
+            Array = "󰅪 ",
+            Object = "󰅩 ",
+            Key = "󰌋 ",
+            Null = "󰟢 ",
+            EnumMember = " ",
+            Struct = "󰌗 ",
+            Event = " ",
+            Operator = "󰆕 ",
+            TypeParameter = "󰊄 ",
+          },
+          lsp = {
+            auto_attach = true,
+            preference = nil,
+          },
+          highlight = false,
+          separator = " > ",
+          depth_limit = 0,
+          depth_limit_indicator = "..",
+          safe_output = true,
+          lazy_update_context = false,
+          click = false,
+          format_text = function(text)
+            return text
+          end,
+        })
+      end,
+    },
   },
   event = "VeryLazy",
   init = function()
@@ -179,8 +230,7 @@ return {
       },
       winbar = {
         lualine_a = { window_number },
-        lualine_b = {},
-        lualine_c = {
+        lualine_b = {
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           {
             "filename",
@@ -191,6 +241,16 @@ return {
               -- newfile = ' ', -- Text to show for new created file before first writting
               newfile = "[new]", -- Text to show for new created file before first writting
             },
+          },
+        },
+        lualine_c = {
+          {
+            function()
+              return require("nvim-navic").get_location()
+            end,
+            cond = function()
+              return require("nvim-navic").is_available()
+            end,
           },
         },
         lualine_x = {},
