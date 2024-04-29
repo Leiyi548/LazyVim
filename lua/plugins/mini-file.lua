c88f4c4e18c9f1d13ac716b2ce17a10b6aa5fee5
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-29 15:00:58 +0800

    使用 mini-file 插件

--- lua/plugins/mini-file.lua
+++ lua/plugins/mini-file.lua
@@ -1,11 +1,33 @@
 return {
   "echasnovski/mini.files",
-  enabled = false,
+  enabled = true,
   opts = {
     windows = {
+      -- Maximum number of windows to show side by side
+      max_number = math.huge,
+      -- Whether to show preview of file/directory under cursor
       preview = true,
-      width_focus = 30,
-      width_preview = 30,
+      -- Width of focused window
+      width_focus = 50,
+      -- Width of non-focused window
+      width_nofocus = 15,
+      -- Width of preview window
+      width_preview = 25,
+    },
+    -- Module mappings created only inside explorer.
+    -- Use `''` (empty string) to not create one.
+    mappings = {
+      close = "q",
+      go_in = "l",
+      go_in_plus = "L",
+      go_out = "h",
+      go_out_plus = "H",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "<C-s>",
+      trim_left = "<",
+      trim_right = ">",
     },
     options = {
       -- Whether to use for editing directories
@@ -15,14 +37,14 @@ return {
   },
   keys = {
     {
-      "<leader>e",
+      "<leader>fm",
       function()
         require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
       desc = "Open mini.files (directory of current file)",
     },
     {
-      "<leader>E",
+      "<leader>fM",
       function()
         require("mini.files").open(vim.loop.cwd(), true)
       end,
@@ -32,6 +54,49 @@ return {
   config = function(_, opts)
     require("mini.files").setup(opts)
 
+    -- Create mappings to modify target window via split
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        -- Make new window and set it as target
+        local new_target_window
+        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+          vim.cmd(direction .. " split")
+          new_target_window = vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target_window)
+      end
+
+      -- Adding `desc` will result into `show_help` entries
+      local desc = "Split " .. direction
+      vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+    -- Create mapping to set current working directory
+    local files_set_cwd = function(path)
+      -- Works only if cursor is on the valid file system entry
+      local cur_entry_path = MiniFiles.get_fs_entry().path
+      local cur_directory = vim.fs.dirname(cur_entry_path)
+      vim.fn.chdir(cur_directory)
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        -- Tweak keys to your liking
+        map_split(buf_id, "gs", "belowright horizontal")
+        map_split(buf_id, "gv", "belowright vertical")
+      end,
+    })
+
+    -- Create mapping to show/hide dot-files
     local show_dotfiles = true
     local filter_show = function(fs_entry)
       return true
@@ -39,7 +104,6 @@ return {
     local filter_hide = function(fs_entry)
       return not vim.startswith(fs_entry.name, ".")
     end
-
     local toggle_dotfiles = function()
       show_dotfiles = not show_dotfiles
       local new_filter = show_dotfiles and filter_show or filter_hide
@@ -55,10 +119,38 @@ return {
       end,
     })
 
+    -- create mapping to set current working directory
+    local files_set_cwd = function(path)
+      -- Works only if cursor is on the valid file system entry
+      local cur_entry_path = MiniFiles.get_fs_entry().path
+      local cur_directory = vim.fs.dirname(cur_entry_path)
+      vim.fn.chdir(cur_directory)
+    end
+
+    -- create mapping to copy current file relative path
+    local yank_relative_path = function()
+      local path = MiniFiles.get_fs_entry().path
+      vim.fn.setreg('"', vim.fn.fnamemodify(path, ":."))
+    end
+
     vim.api.nvim_create_autocmd("User", {
-      pattern = "MiniFilesActionRename",
-      callback = function(event)
-        require("lazyvim.util").lsp.on_rename(event.data.from, event.data.to)
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        vim.keymap.set("n", "gY", yank_relative_path, { buffer = args.data.buf_id })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        -- save to sync mini-file buffer
+        vim.keymap.set({ "i", "x" }, "<C-s>", function()
+          require("mini.files").synchronize()
+        end, { buffer = args.data.buf_id })
+        -- use ctrl-c to quit mini-file buffer
+        vim.keymap.set({ "i", "x" }, "<C-s>", function()
+          require("mini.files").synchronize()
+        end, { buffer = args.data.buf_id })
       end,
     })
   end,
