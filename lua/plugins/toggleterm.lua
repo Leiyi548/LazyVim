3b910ac00eed65e28bf85000535c2b8677d17e85
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-04-30 23:03:15 +0800

    C-/ toggle 终端使用 toggleterm 插件

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,6 +12,7 @@ return {
     {"<F2>","<cmd>ToggleTerm direction=tab<cr>",mode = {"n","x","i","t"},desc= "toggleterm in new tab"},
     {"<F5>","<cmd>ToggleTerm direction=horizontal<cr>",mode = {"n","x","i","t"},desc= "toggleterm horizontal split"},
     {"<F6>","<cmd>ToggleTerm direction=vertical size=40<cr>",mode = {"n","x","i","t"},desc= "toggleterm vertical split"},
+    {"<C-/>","<cmd>ToggleTerm direction=float<cr>",mode = {"n","x","i","t"},desc= "toggleterm horizontal float"},
     -- stylua: ignore end
   },
 }
