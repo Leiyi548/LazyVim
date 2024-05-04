eafada4195aa441f2a84458f88501a9d7449daa0
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-05-04 11:45:07 +0800

    C-m 不能当作 toggleterm 键改成了 C-3

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -9,7 +9,7 @@ return {
   },
   keys = {
     -- stylua: ignore start
-    {"<C-m>","<cmd>ToggleTerm direction=tab<cr>",mode = {"n","x","i","t"},desc= "toggleterm in new tab"},
+    {"<C-3>","<cmd>ToggleTerm direction=tab<cr>",mode = {"n","x","i","t"},desc= "toggleterm in new tab"},
     {"<C-j>","<cmd>ToggleTerm direction=horizontal<cr>",mode = {"n","x","i","t"},desc= "toggleterm horizontal split"},
     {"<C-l>","<cmd>ToggleTerm direction=vertical size=40<cr>",mode = {"n","x","i","t"},desc= "toggleterm vertical split"},
     {"<C-2>","<cmd>ToggleTerm direction=float<cr>",mode = {"n","x","i","t"},desc= "toggleterm horizontal float"},
