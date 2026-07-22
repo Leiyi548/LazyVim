a83e90f3968451c02126c0131822ca0706910e7b
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2026-07-23 00:29:03 +0800

    安装配置 everforest 主题，自定义flash.nvim高亮

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,16 @@ return {
     "neanias/everforest-nvim",
     version = false, -- 始终使用最新的版本
     lazy = true,
-    opts = {
-      background = "hard",
-    },
+    config = function ()
+      require("everforest").setup({
+        --- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
+        --- Default is "medium".
+        background = "hard",
+        on_highlights = function (hl,palette)
+          -- flash
+          hl.FlashLabel = {fg="#222222", bg=palette.green, bold = true }
+        end
+      })
+    end
   },
 }
