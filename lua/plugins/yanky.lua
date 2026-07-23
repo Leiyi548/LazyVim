39e1df872b289297e7e5893c0b1bceb6dd8735c4
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2026-07-23 23:02:29 +0800

    feat(yanky): 增加yanky体验，取消snacks picker 的预览和进入就是 normal 模式
    增加yanky

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,39 @@
+return {
+  "gbprod/yanky.nvim",
+  keys = {
+    {
+      "<leader>p",
+      function()
+        Snacks.picker.yanky({
+          focus = "list",
+          -- preset = "default" 是居中布局，但默认 0.8x0.8 偏大；
+          -- 内层再嵌 layout = {...} 覆盖尺寸为 vscode 同款 0.4x0.4（深度合并保留 default 的居中盒子）
+          layout = {
+            preview = false,
+            preset = "default",
+          },
+        })
+      end,
+      mode = { "n", "x" },
+      desc = "Open Yank History",
+    },
+        -- stylua: ignore
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
+    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
+    { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Through Yank History" },
+    { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
+    { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
+    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
+    { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
+    { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
+    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and Indent Right" },
+    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and Indent Left" },
+    { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put Before and Indent Right" },
+    { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put Before and Indent Left" },
+    { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put After Applying a Filter" },
+    { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put Before Applying a Filter" },
+  },
+}
