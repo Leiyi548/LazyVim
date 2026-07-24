c66e082a4d87eb7f47bc9f466e0dfc85aa6c6f19
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2026-07-24 21:38:30 +0800

    feat(neogit):  添加neogit sections配置，commit 区域不折叠

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,6 +8,53 @@ return {
     },
     disable_line_numbers = false,
     disable_relative_line_numbers = false,
+    sections = {
+      -- Reverting/Cherry Picking
+      sequencer = {
+        folded = false,
+        hidden = false,
+      },
+      untracked = {
+        folded = false,
+        hidden = false,
+      },
+      unstaged = {
+        folded = false,
+        hidden = false,
+      },
+      staged = {
+        folded = false,
+        hidden = false,
+      },
+      stashes = {
+        folded = true,
+        hidden = false,
+      },
+      unpulled_upstream = {
+        folded = true,
+        hidden = false,
+      },
+      unmerged_upstream = {
+        folded = false,
+        hidden = false,
+      },
+      unpulled_pushRemote = {
+        folded = true,
+        hidden = false,
+      },
+      unmerged_pushRemote = {
+        folded = false,
+        hidden = false,
+      },
+      recent = {
+        folded = false,
+        hidden = false,
+      },
+      rebase = {
+        folded = true,
+        hidden = false,
+      },
+    },
     mappings = {
       status = {
         ["s"] = false,
